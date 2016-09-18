d64233d26a802a6b9d8ff825613d0debb6c604f9
Stefan Hartmann <stefanhartmann@gmail.com>
2016-09-18 15:34:42 -0400

    add javascript to index

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,14 @@
 "use strict";
 (function(){
 
+$.ajax({url:"http://localhost:9292/album_status"}).done(function(data){
+  if (data.new_album === "no") {
+    $('p').text("No new album :(")
+  } else if (data.new_album === "yes"){
+    {
+      $('p').text("New album!")
+    }
+  }
+})
+
 })();
